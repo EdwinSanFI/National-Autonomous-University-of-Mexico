debde54a0d43d6563a05bcad364c0a6488ac72df
Edwin Santiago <jaret-@live.com.mx>
2022-09-24 16:31:51 -0500

    Changing the readme practice 0

--- Third Semester/Data Structures and Algorithms II/Practice 0/Fibonaccis.py
+++ Third Semester/Data Structures and Algorithms II/Practice 0/Fibonaccis.py
@@ -4,38 +4,32 @@
 # graficas conforme a los datos arrojados
 ##################################
 
-# Se importa de la libreria "time" la funcion "perf_counter"
-# para calcular el tiempo que tarda en obtener el numero n de fibonacci
-# en distintos algoritmos
 from time import perf_counter
 
-# Se importa la libreria matplotlib.pyplot par graficar
 import matplotlib.pyplot as plt
 
-# Se inicializa 4 lista en donde se guardará:
-    # Los numeros 0 hasta el N en el algoritmo Fibonacci Recursivo
-x_recursivo = []
-    # El tiempo de ejecución de cada numero en el algorimto Fibonacci Recursivo
-y_recursivo = []
-    # Los numeros 0 hasta el N en el algoritmo Fibonacci Iteractivo
-x_iteractivo = []
-    # El tiempo de ejecución de cada numero en el algorimto Fibonacci Iteractivo
-y_iteractivo = []
+# X = Fibonacci number
+# Y = Time of execution in seconds
+
+x_recursive = []
+y_recursive = []
+
+x_iterative = []
+y_iterative = []
 
 """
-> The function calls itself twice, and each time it calls itself, it calls itself twice again, and
-so on
+> The function returns the sum of the two previous numbers in the sequence
 
-:param n: The number of times the function is called
-:return: The nth number of the Fibonacci sequence
+:param n: the number of fibonacci numbers to generate
+:return: The nth number in the fibonacci sequence.
 """
-def fibo_recursivo(n):
+def fibo_recursive(n):
     if n == 0 or n == 1:
         return 1
-    return fibo_recursivo(n-1) + fibo_recursivo(n-2) #* O(n^c)
+    return fibo_recursive(n-1) + fibo_recursive(n-2) # O(n^c)
 
-#* 2 + n^c = O(2 + n^c) = O(n^c)
-#* Complejidad exponencial O(n^c)
+# 2 + n^c = O(2 + n^c) = O(n^c)
+# Exponential complexity O(n^c)
 
 """
 It takes the sum of the previous two numbers in the sequence and returns the nth number in the
@@ -45,101 +39,77 @@ sequence
 :return: The nth number of the Fibonacci sequence
 """
 def fib_iterativo(n):
-    f0 = 0 #* O(1)
-    f1 = 1 #* O(1)
+    f0 = 0   # O(1)
+    f1 = 1   # O(1)
     if n == 0 or n == 1:
         return 1
-    for _ in range(2, n+1): #* O(n)
-        fn = f0 + f1 #* O(1) * n
-        f0 = f1 #* O(1) * n
-        f1 = fn #* O(1) * n
-    return fn #* O(n)
+    for _ in range(2, n+1):    # O(n)
+        fn = f0 + f1    # O(1) * n
+        f0 = f1    # O(1) * n
+        f1 = fn    # O(1) * n
+    return fn    # O(n)
 
-#* 4 + 3n = O(4 + 3n) = O(n)
-#* Complejidad Lineal O(n)
+# 4 + 3n = O(4 + 3n) = O(n)
+# Lineal Complexity O(n)
 
-
-#Se calcula el tiempo que tarda en realizar las operaciones
-def calcularTiempo():
-    # Es la posicion del numero a calcular
+def calculate_time():
     n = 40
 
-    #* Se calcula el fibonacci Recursivo
-    print("\n**Fibonacci Recursivo**")
+    print("\n**Fibonacci recursive**")
     for i in range (0, n+1):
-        # Se inicializa el cronómetro
-        tiempoInicial = perf_counter()
-        # Se ejecuta el algoritmo
-        fibb_rec = fibo_recursivo(i)
-        # Se detiene el cronómetro
-        tiempoFinal = perf_counter()
-        #Se calcula el tiempo tomado a completar el algoritmo
-        tiempo = tiempoFinal - tiempoInicial
+        # Start the timer
+        initial_time = perf_counter()
+        fibb_rec = fibo_recursive(i)
+        # Stop the timer
+        final_time = perf_counter()
+        tiempo = final_time - initial_time
 
-        # Se agrega a la lista el numero calculado
-        x_recursivo.append(i)
-        # Se agrega a la lista el tiempo que tardó el programa en calcularlo
-        y_recursivo.append(tiempo)
+        x_recursive.append(i)
+        y_recursive.append(tiempo)
 
-        # Se imprimen los datos
-        print(f'El numero {i} de Fibo es {fibb_rec} y tardó: {tiempo}[s]')
+        print(f'The Fibonacci number of {i} is {fibb_rec} and took: {tiempo}[s]')
     print("\n")
 
-    #* Se calcula el fibonacci Iterativo
     print("**Fibonacci Iterativo**")
     for i in range (0, n+1):
-        # Se inicializa el cronómetro
-        tiempoInicial = perf_counter()
-        # Se ejecuta el algoritmo
+        initial_time = perf_counter()
         fibb_ite = fib_iterativo(i)
-        # Se detiene el cronómetro
-        tiempoFinal = perf_counter()
-        #Se calcula el tiempo tomado a completar el algoritmo
-        tiempo = tiempoFinal - tiempoInicial
+        final_time = perf_counter()
+        tiempo = final_time - initial_time
 
-        # Se agrega a la lista el numero calculado
-        x_iteractivo.append(i)
-        # Se agrega a la lista el tiempo que tardó el programa en calcularlo
-        y_iteractivo.append(tiempo)
+        x_iterative.append(i)
+        y_iterative.append(tiempo)
 
-        # Se imprimen los datos
         print(f'El numero {i} de Fibo es {fibb_ite} y tardó: {tiempo}[s]')
     print("\n")
 
-# Se grafica los datos obtenidos
 def graficar():
-    #* Fibonacci Recursivo
-    # Se grafica en el eje X los numeros del 0 a "n"
-    # Se grafica en el eje y el tiempo de ejecucion de cada numero
-    plt.scatter(x_recursivo, y_recursivo)
-    # Se le asigna un titulo en el eje X
+    # Fibonacci recursive
+    plt.scatter(x_recursive, y_recursive)
+    # Title of the X axis
     plt.xlabel("N")
-    # Se le asigna un titulo en el eje Y
+    # Title of the Y axis
     plt.ylabel("Segundos[s]")
-    # Se le asigna un titulo con caracteristicas
-    plt.title("Fibonacci Recursivo", fontdict={'family': 'serif', 'color': 'red', 'size': 20})
-    # Se cuadricula la grafica
+    # Title of the graph
+    plt.title("Fibonacci recursive", fontdict={'family': 'serif', 'color': 'red', 'size': 20})
+    # Add grid
     plt.grid(True)
-    # Se genera la grafica en forma de imagen
+    # Generate the image
     plt.show()
 
-    #*  Fibonacci Iteractivo
-    # Se grafica en el eje X los numeros del 0 a "n"
-    # Se grafica en el eje y el tiempo de ejecucion de cada numero
-    plt.scatter(x_iteractivo, y_iteractivo)
-    # Se le asigna un titulo en el eje X
+    # Fibonacci iterative
+    plt.scatter(x_iterative, y_iterative)
+    # Title of the X axis
     plt.xlabel("N")
-    # Se le asigna un titulo en el eje Y
+    # Title of the Y axis
     plt.ylabel("Segundos[s]")
-    # Se le asigna un titulo con caracteristicas
-    plt.title("Fibonacci Iteractivo", fontdict={'family': 'serif', 'color': 'red', 'size': 20})
-    # Se cuadricula la grafica
+    # Title of the graph
+    plt.title("Fibonacci iterative", fontdict={'family': 'serif', 'color': 'red', 'size': 20})
+    # Add grid
     plt.grid(True)
-    # Se genera la grafica en forma de imagen
+    # Generate the image
     plt.show()
 
-#Aqui inicia la ejecucion del programa el programa
 if __name__ == '__main__':
-    # Se ejecuta la funcion para tomar el tiempo
-    calcularTiempo()
+    calculate_time()
     graficar()
--- Third Semester/Data Structures and Algorithms II/Practice 0/README.md
+++ Third Semester/Data Structures and Algorithms II/Practice 0/README.md
@@ -1,4 +1,45 @@
-## Fibonacci
+## Fibonacci sequence
+
+Is the sequence of numbers where each number is the sum of the two preceding ones, starting from 0 and 1. The "F_n" is the n-th number in the sequence and the formula is:
+
+```math
+F_0 = 0
+F_1 = 1
+
+F_n = F_{n-1} + F_{n-2}
+```
+
 <p align="center">
   <img src="https://user-images.githubusercontent.com/6312342/165682339-436a19de-994d-4366-ae0a-bb533000af22.png">
+</p>
+
+For this practice you need to use the Virtual Environment to install **matplotlib** and run the code.
+
+### Iterative Fibonacci
+
+
+```python
+def fib(n):
+    """Return the n-th number in the Fibonacci sequence."""
+    pass
+```
+
+<p align="center">
+  <img src="./Fibonacci Iteractivo.png">
+</p>
+
+
+### Recursive Fibonacci
+
+Time complexity: O(2^n)
+
+```python
+def fibo_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    return fibo_recursive(n-1) + fibo_recursive(n-2) # O(n^c)
+```
+
+<p align="center">
+  <img src="./Fibonacci Recursivo.png">
 </p>
