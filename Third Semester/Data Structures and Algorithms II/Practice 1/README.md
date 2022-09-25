820a6d2b395495e0fc861e9982756a731ce2b359
Edwin Santiago <jaret-@live.com.mx>
2022-09-25 12:13:16 -0500

    Adding readme on practice 1

--- Third Semester/Data Structures and Algorithms II/Practice 1/README.md
+++ Third Semester/Data Structures and Algorithms II/Practice 1/README.md
@@ -1,1 +1,65 @@
-## Sorting Algorithms Part I
+## Sorting Algorithms Part 1
+
+### Bubble Sort 
+
+Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted. The algorithm gets its name from the way smaller elements "bubble" to the top of the list. Because it only uses comparisons to operate on elements, it is a comparison sort. Although the algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort. It can be practical if the input is usually in sort order but may occasionally have some out-of-order elements nearly in position.
+
+python code:
+
+```python
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+```
+
+### Buble Sort Optimized
+
+```python
+def bubble_sort_optimized(arr):
+    for i in range(len(arr)):
+        swapped = False
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+```
+
+### Merge Sort
+
+Merge sort is a divide and conquer algorithm based on the idea of breaking down a list into several sub-lists until each sublist consists of a single element and merging those sublists in a manner that results into a sorted list.
+
+python code:
+
+```python
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr
+```
--- Third Semester/Data Structures and Algorithms II/Practice 1/algoritmosDeOrdenamiento.py
+++ Third Semester/Data Structures and Algorithms II/Practice 1/algoritmosDeOrdenamiento.py
@@ -1,83 +1,69 @@
 ##################################
 # @author: Edwin Santiago
-# Se implementan los 3 algoritmos (BubbleSort, BubbleSortOptimizado y MergeSort)
-# Con una lista de 10 números aleatorios del 0 al 10
+# 3 Algorithms for sorting numbers
 ##################################
 
-# Se importa de la libreria "time" la funcion "perf_counter"
-# para calcular el tiempo que tardan en ordenar cada algoritmo
 from time import perf_counter
 
-# Algoritmo BubbleSort
-# Recibe como parametro el arreglo de 10 numeros
-def bubbleSort(arr):
-    # Se obtiene el tamaño del arreglo
+def bubble_sort(arr):
     n = len(arr)
-    # Se recorre el arreglo, esto es para repetir las instrucciones siguientes
-    # Y ordenar el arreglo
     for i in range(n-1):
-        # Se vuelve a recorrer el arreglo
         for j in range(n - i - 1):
-            # Si la posicion siguiente de la actual es menor
+            # If the next element is smaller than the current element
             if arr[j] > arr[j+1]:
-                # Estos cambian de posicion
+                # Swap the elements
                     arr[j], arr[j+1] = arr[j+1], arr[j]
 
-# Algoritmo BubbleSortOptimizado
-# Recibe como parametro el arreglo de 10 numeros
-def bubbleSortOptimizado(arr):
-    # Se calcula el tamaño del arreglo
+# Bubble Sort Optimized
+def bubble_sort_optimized(arr):
     n = len(arr)
-    # Se inicializan valores
     i = 0
     ordenado = False
-    # La sentencia while evita que se hagan instrucciones demas
-    # Como lo hae el algoritmo BubbleSort
     while (i < n and ordenado == False):
         i += 1
         ordenado = True
-        # Recorrera la lista completa
         for j in range(n - i):
-            # Si la posicion siguiente de la actual es menor
+            # If the next element is smaller than the current element
             if arr[j] > arr[j+1]:
                 ordenado = False
-                # Estos cambian de posicion
+                # Swap the elements
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
-# Algoritmo MergeSort
-# Recibe el arreglo, el indice incial y el indice final
-def mergeSort(arr, p, r):
-    # Al ser un algoritmo recursivo, se revisa que el arreglo
-    # Tenga mas de 1 valor con la posicion inicial y final
+# Merge Sort
+"""
+If the array has more than one element, split it into two halves, sort each half, and then merge the
+two sorted halves
+
+:param arr: The array to be sorted
+:param p: the start index of the array
+:param r: the last index of the array
+"""
+def merge_sort(arr, p, r):
+    # If the array has more than one element, split it into two halves, sort each half, and then merge the two sorted halves
     if p < r:
-        # Se parte en dos el arrelgo
         q = int((p+r)/2)
-        # Se llama a si misma con la mitad izquierda del arreglo
-        mergeSort(arr, p, q)
-        # Se llama a si misma con la mitad derecha del arreglo
-        mergeSort(arr, q+1, r)
-        # Una vez llegado a solo listas con 1 dato, estas se acomodan y se mezclan.
+        # From the initial position to the middle position
+        merge_sort(arr, p, q)
+        # From the middle position to the final position
+        merge_sort(arr, q+1, r)
+        # Merge and sort the array
         merge(arr,p,q,r)
 
-# Es parte del algoritmo MergeSort
-# Mezcla y ordena los elementos
+# Merge and sort the array
 # Recibe los elementos, el indice incial, el de medio y el final.
 def merge(arr, p, q, r):
-    # Garda un valor en la variable izq
     izq = arr[p:q+1]
-    # Garda un valor en la variable der
     der = arr[q+1:r+1]
     i = 0
     j = 0
-    # Recorre desde el indie inicial hasta el indice final del arreglo
+    # From the initial position to the final position
     for k in range(p, r+1):
-        # Comprueba que valor de que variable es menor
         if (j >= r-q) or ((i<q - p+1) and (izq[i] < der[j])):
-            # Si la vairable izq es menor, se asigna en el arrelgo
+            # If the left element is smaller than the right element will be placed in the array
             arr[k] = izq[i]
             i += 1
         else:
-            # Si la variable der es menor, se asigna en el arreglo
+            # If the right element is smaller than the left element will be placed in the array
             arr[k] = der[j]
             j += 1
 
@@ -87,55 +73,34 @@ def run():
     arreglo2 = [3, 5, 0, 9, 7, 4, 1, 6, 8, 2]
     arreglo3 = [3, 5, 0, 9, 7, 4, 1, 6, 8, 2]
 
-    print("La lista de numeros a ordenar es: " + str(arreglo1) + "\n")
+    print("The number list is: " + str(arreglo1) + "\n")
 
-    # Se toman los tiempos de cada algoritmo
-
-    #! Algoritmo 1
-    # Se inicializa el cronómetro
+    # Algorithm 1
     tiempoInicial1 = perf_counter()
-    # Se ejecuta el algoritmo
-    bubbleSort(arreglo1)
-    # Se detiene el cronómetro
+    bubble_sort(arreglo1)
     tiempoFinal1 = perf_counter()
-    #Se calcula el tiempo tomado a completar el algoritmo
     tiempo1 = tiempoFinal1 - tiempoInicial1
-    # Se imprimen los valores
     print('Arreglo ordenado: ', arreglo1)
     # {:.9f} es para indiciarle cuantos decimales se desea imprimir
-    print("El tiempo que le tomó al algoritmo BubbleSort es de: {:.9f}\n".format(tiempo1))
+    print("El tiempo que le tomó al algoritmo bubble_sort es de: {:.9f}\n".format(tiempo1))
 
-    #! Algoritmo 2
-    # Se inicializa el cronómetro
+    #! Algorithm 2
     tiempoInicial2 = perf_counter()
-    # Se ejecuta el algoritmo
-    bubbleSortOptimizado(arreglo2)
-    # Se detiene el cronómetro
+    bubble_sort_optimized(arreglo2)
     tiempoFinal2 = perf_counter()
-    #Se calcula el tiempo tomado a completar el algoritmo
     tiempo2 = tiempoFinal2 - tiempoInicial2
-    # Se imprimen los valores
     print('Arreglo ordenado: ', arreglo2)
     #{:.9f} es para indiciarle cuantos decimales se desea imprimir
-    print("El tiempo que le tomó al algoritmo BubbleSortOptimizado es de: {:.9f}\n".format(tiempo2))
+    print("El tiempo que le tomó al algoritmo bubble_sort_optimized es de: {:.9f}\n".format(tiempo2))
 
-    #! Algoritmo 3
-    # Se inicializa el cronómetro
+    #! Algorithm 3
     tiempoInicial3 = perf_counter()
-    # Se ejecuta el algoritmo
-    mergeSort(arreglo3,0,9)
-    # Se detiene el cronómetro
+    merge_sort(arreglo3,0,9)
     tiempoFinal3 = perf_counter()
-    #Se calcula el tiempo tomado a completar el algoritmo
     tiempo3 = tiempoFinal3 - tiempoInicial3
-    # Se imprimen los valores
     print('Arreglo ordenado: ', arreglo3)
     #{:.9f} es para indiciarle cuantos decimales se desea imprimir
-    print("El tiempo que le tomó al algoritmo MergeSort es de: {:.9f}\n".format(tiempo3))
+    print("El tiempo que le tomó al algoritmo merge_sort es de: {:.9f}\n".format(tiempo3))
 
 if __name__ == '__main__':
-    # Se imprime el numero de la practica y del ejercicio
-    print('\t¡Practica 1!')
-    print('\tEjercicio 2\n')
-    # Se ejecuta la funcion donde ejecuta otras funciones
     run()
